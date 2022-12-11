2daec627f082895bc5381a88c230c30670433dc2
xenia <xhartmann@posteo.de>
2022-12-11 17:45:53 +0100

    adapt heading desing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require('tailwindcss/plugin')
 
 module.exports = {
   content: [
@@ -16,9 +17,26 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-				sans: ['Impact', ...defaultTheme.fontFamily.sans],
-			},
+        sans: ['Overpass', ...defaultTheme.fontFamily.sans],
+      },
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+        xl: 'black 5px 5px 2px'
+      }
     },
   },
-  plugins: []
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ]
 }
